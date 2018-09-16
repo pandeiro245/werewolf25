5caffc9b8371e074dae8ff494cd50a112c45cd5a
西小倉宏信 <nishiko@mindia.jp>
2018-09-16 18:11:09 +0000

    Twitter Login

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,16 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+  def callback
+    data = request.env['omniauth.auth']
+    user = User.find_or_create_by(
+      twitter_id: data[:uid]
+    )
+    user.name = data[:info].name
+    user.screen_name = data[:info].nickname
+    user.save
+    cookies[:user_id] = user.id
+    redirect_to :root
+  end
 end
