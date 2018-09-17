9c4c477643ee80eb26a1340a7debf901cd502b21
西小倉宏信 <nishiko@mindia.jp>
2018-09-17 09:01:18 +0000

    show my role refs #5

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,6 +1,8 @@
 class RoomsController < ApplicationController
   def show
+    @room = Room.find(params[:id])
     @comment = Comment.new
     @comments = Comment.where(room_id: params[:id]).order('id desc')
+    @role = Member.find_by(room_id: params[:id], user_id: cookies[:user_id]).role
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-Room.create!
+Room.create! if Room.count.zero?
+role = Role.find_or_create_by(name: 'gm')
+Member.create!({room: Room.first, user: user, role: role})
+
+Role.find_or_create_by(name: 'werewolf')
+Role.find_or_create_by(name: 'citizen')
+
